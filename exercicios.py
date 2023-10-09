125cd6a336e393a45c6bdcad94129b8a2f541440
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2023-10-09 12:33:00 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -973,7 +973,25 @@ print(f'O total da compra foi de R${total:.2f}, {mais1k} produtos valem mais de 
 '''
 
 #exercicio 71
-
+'''
 saque = int(input('Qual valor a ser sacado: '))
-
-
+ced = 50
+totced = 0
+while True:
+    if saque >= ced:
+        saque -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cedulas de {ced}')
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if saque == 0:
+            break
+print('Finalizado')
+'''
