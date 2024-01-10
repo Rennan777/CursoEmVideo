312af491bac314bb902285048cdf502b44049b70
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2024-01-10 11:55:11 -0300

    exercicio 91

--- exercicios.py
+++ exercicios.py
@@ -1419,5 +1419,24 @@ for k, v in aluno.items():
 '''
 
 #exercicio 91
+'''
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogadores = {'jogador1': randint(1, 6),
+             'jogador2': randint(1, 6),
+             'jogador3': randint(1, 6),
+             'jogador4': randint(1, 6)}
+rank = list()
+print('*-' * 15)
+for k, v in jogadores.items():
+    print(f'O {k}, tirou {v} no dado.')
+    sleep(0.5)
+rank = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+print('*-* RANKING *-*')
+for i, v in enumerate(rank):
+    print(f'{i + 1}o lugar: {v[0]}, com {v[1]} pontos.')
+    sleep(0.5)
+'''
 
-
+#exercicio 92
