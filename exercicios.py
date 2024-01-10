166ea98386fdb44507ea8051f0435eb56726492d
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2024-01-10 10:16:36 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1401,4 +1401,23 @@ while True:
 '''
 
 #exercicio 90
+'''
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
+if aluno['media'] <= 5:
+    aluno['status'] = 'Reprovado'
+if aluno['media'] <= 7:
+    aluno['status'] = 'Recuperacao'
+else:
+    aluno['status'] = 'Aprovado'
+
+print('=-' * 10)
+#print(f'- Nome é {aluno["nome"]}\n- Media é {aluno["media"]}\n- Situação é {aluno["status"]}')
+for k, v in aluno.items():
+    print(f'{k} é {v}')
+'''
+
+#exercicio 91
+
 
