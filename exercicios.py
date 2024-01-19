7387d18775023429bd6f92abeb5fdff420d2217f
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2024-01-19 16:32:49 -0300

    exercicio 104

--- exercicios.py
+++ exercicios.py
@@ -1801,5 +1801,27 @@ def ficha(nome='<Desconhecido>', gols=0):
 print(ficha())
 '''
 
-#exercicio 104
+
+# exercicio 104
+'''
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[31mErro. Digite um numero valido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+n = leiaint('Digite um numero: ')
+print(f'Voce digitou o numero {n}')
+'''
+
+# exercicio 105
 
