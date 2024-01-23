14c42f5aa589d1ca0028d828f7f4bd660d7b34dc
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2024-01-23 16:22:41 -0300

    ajustes modulos

--- exercicios.py
+++ exercicios.py
@@ -1906,5 +1906,5 @@ print(f'A metade de R${valor} é R${moeda.metade(valor)}.')
 print(f'O dobro de R${valor} é R${moeda.dobro(valor)}')
 print(f'Aumentando 10%, temos R${moeda.aumentar(valor)}')
 print(f'Diminuindo 10%, temos R${moeda.diminuir(valor)}')
-
 '''
+
