2e653148107c055bcbd16230d80f1c8e81b9cc93
Rennan777 <114919815+Rennan777@users.noreply.github.com>
2024-01-23 16:16:26 -0300

    exercicio 107

--- exercicios.py
+++ exercicios.py
@@ -1896,3 +1896,15 @@ def ajuda():
 ajuda()
 '''
 
+# exercicio 107
+'''
+import moeda
+
+valor = float(input('Digite um valor: R$'))
+
+print(f'A metade de R${valor} é R${moeda.metade(valor)}.')
+print(f'O dobro de R${valor} é R${moeda.dobro(valor)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(valor)}')
+print(f'Diminuindo 10%, temos R${moeda.diminuir(valor)}')
+
+'''
---
+++ moeda.py
@@ -0,0 +1,17 @@
+def aumentar(valor, percent=10):
+    percent = (percent / 100) * valor
+    return valor + percent
+
+
+def diminuir(valor, percent=10):
+    percent = (percent / 100) * valor
+    return valor - percent
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def metade(valor):
+    return valor / 2
+
